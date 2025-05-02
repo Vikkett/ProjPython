c2cc192bbcfbe4e0146ab63c7d76fac4145a1d41
hev0s <niels.delafontaine@eduvaud.ch>
2025-05-02 13:31:18 +0200

    Added data for pilots_has_results

---
+++ data_base/pilots_has_results.sql
@@ -0,0 +1,8 @@
+USE mydb;
+INSERT INTO pilots_has_results (Pilots_id, results_id, position, TIME)
+VALUES 
+	(1,1, 3,'03:46:24' ),
+	(2,1, 5, '03:48:09'),
+	(3,1, 2, '03:46:19'),
+	(4,1, 4, '03:47:32'),
+	(5,1, 1, '03:45:30');
